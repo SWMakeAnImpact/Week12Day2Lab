28b4a045794c142860887f3ee47326dd48469995
SWMakeAnImpact <wilkessarah96@gmail.com>
2023-12-06 23:50:31 -0500

    Add Magic 8 Ball

--- server.js
+++ server.js
@@ -23,3 +23,30 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     // Respond to the client with the calculated tip amount
     res.send(`Your tip should be \$${tip.toFixed(2)}`);
   });
+
+  // Magic 8 Ball Route
+// Ask a question in the URL and get a random Magic 8 Ball response.
+const magic8Responses = [
+    // Responses array
+    "It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", 
+    "You may rely on it", "As I see it yes", "Most likely", "Outlook good",
+    "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later",
+    "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+    "Don't count on it", "My reply is no", "My sources say no",
+    "Outlook not so good", "Very doubtful"
+  ];
+  
+  app.get('/magic/:question', (req, res) => {
+    // Format question from URL
+    const question = req.params.question.replace(/%20/g, " ");
+    // Get random answer
+    const answer = magic8Responses[Math.floor(Math.random() * magic8Responses.length)];
+    // Send formatted question and answer
+    res.send(`<h1>${question}? ${answer}</h1>`);
+  });
+  
+  // Start server
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  
