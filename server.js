94cf2ea9521617ee858d6cd77c9bf8eb6a2089fe
SWMakeAnImpact <wilkessarah96@gmail.com>
2023-12-06 22:46:58 -0500

    Add tip calculator route

--- server.js
+++ server.js
@@ -11,3 +11,15 @@ app.get('/greeting/:name', (req, res) => {
     const name = req.params.name;
     res.send(`Hello, ${name}! It's so great to see you!`);
   });
+
+// Tip Calculator Route
+// This route calculates the tip based on a total bill amount and a tip percentage.
+// It expects two parameters in the URL: 'total' for the bill amount and 'tipPercentage' for the tip.
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    const total = parseFloat(req.params.total);
+    const tipPercentage = parseFloat(req.params.tipPercentage);
+    // Multiply the total bill amount by the tip percentage and dividing by 100
+    const tip = (total * tipPercentage) / 100;
+    // Respond to the client with the calculated tip amount
+    res.send(`Your tip should be \$${tip.toFixed(2)}`);
+  });
